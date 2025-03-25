f6f491619fee014821e9bc520e2c438fab2739d7
krystian-laubach-jr <krystian.laubach.jr@gmail.com>
2025-03-25 16:42:39 +0000

    make categories generate from database ; add missing images

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view( '' , 'home')->name('home');
+Route::get('' , function () {
+  $categories = DB::table('categories')->get();
+  return view(  'home', ['categories' => $categories ]);
+})->name('home');
+
 Route::view( 'about' , 'about')->name('about');
 Route::view( 'contact' , 'contact')->name('contact');
 Route::view('article', 'article')->name('article');
