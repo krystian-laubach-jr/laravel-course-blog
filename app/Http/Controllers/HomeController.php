32eb7d0fb3684cb49de58f690eef0476515a0733
krystian-laubach-jr <krystian.laubach.jr@gmail.com>
2025-03-26 10:56:11 +0000

    make posts generate from database by category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $posts = Post::where('category_id', request('category_id'))
+            ->orderBy('id','desc')->get();
+
         $categories = Category::all();
 
-        return view(  'home', ['categories' => $categories ]);
+        return view(  'home', compact('categories', 'posts') );
     }
 }
